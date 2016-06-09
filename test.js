d30c23b873d694338145598bc18d0b3f6e67fb32
velocityzen <velocityzen@gmail.com>
2016-06-09 15:41:33 -0400

    Added youtube link test

--- test.js
+++ test.js
@@ -11,3 +11,7 @@ extract({ uri: 'http://www.w3.org/TR/html4/index/list.html' }, (err, res) =>
 extract({ uri: 'http://65.media.tumblr.com/6befa1e9d561cb2c66e65a67731af600/tumblr_o7t3afkeA41qagn9ao1_1280.jpg' }, (err, res) =>
   console.log(err, res)
 );
+
+extract({ uri: 'https://www.youtube.com/watch?v=9M77quPL3vY&list=RDEMhe2AFH_WvB5nuMd9tU5CHg&index=27' }, (err, res) =>
+  console.log(err, res)
+);
