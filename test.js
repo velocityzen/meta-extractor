48761edb4a9b6979aad3dacc850547abc578823d
Joon Ho Cho <joonho1101@gmail.com>
2016-07-16 09:34:35 -0700

    handle redirect; support <base> and <link> tags

--- test.js
+++ test.js
@@ -1,17 +1,24 @@
 const extract = require('./index');
 
-extract({ uri: 'http://www.newyorker.com' }, (err, res) =>
-  console.log(err, res)
-);
+const uris = [
+  'http://www.newyorker.com',
+  'http://www.w3.org/TR/html4/index/list.html',
+  'http://65.media.tumblr.com/6befa1e9d561cb2c66e65a67731af600/tumblr_o7t3afkeA41qagn9ao1_1280.jpg',
+  'https://www.youtube.com/watch?v=9M77quPL3vY&list=RDEMhe2AFH_WvB5nuMd9tU5CHg&index=27',
+  'https://medium.com/@joonhocho/getting-off-the-google-bus-24b8ecc12328',
+  'http://joonhocho.com',
+];
 
-extract({ uri: 'http://www.w3.org/TR/html4/index/list.html' }, (err, res) =>
-  console.log(err, res)
-);
-
-extract({ uri: 'http://65.media.tumblr.com/6befa1e9d561cb2c66e65a67731af600/tumblr_o7t3afkeA41qagn9ao1_1280.jpg' }, (err, res) =>
-  console.log(err, res)
-);
-
-extract({ uri: 'https://www.youtube.com/watch?v=9M77quPL3vY&list=RDEMhe2AFH_WvB5nuMd9tU5CHg&index=27' }, (err, res) =>
-  console.log(err, res)
-);
+uris.forEach((uri) => {
+  extract({
+    uri,
+    headers: {
+      Accept: 'text/html',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+    },
+    timeout: 10 * 1000
+  }).then(
+    (res) => console.log(uri, 'OK', res),
+    (err) => console.error(uri, 'ERROR', err)
+  );
+});
